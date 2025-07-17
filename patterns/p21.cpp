b7271d4790ef5b3a587793071641819151bb01ab
Devansh Trivedi <trivedidevansh1080@gmail.com>
2025-07-17 15:29:59 +0530

    added p21

--- patterns/p21.cpp
+++ patterns/p21.cpp
@@ -6,22 +6,39 @@ int main() {
     int n;
     cout<<"enter pattern number : ";
     cin>>n;
-    cout<<string(2*n-1,'0'+n)<<endl;
-    for(int i=0;i<(2*n-1)-2;i++){
+    int l,r,t,d;
+    for(int i=0;i<2*n-1;i++){
         for(int j=0;j<2*n-1;j++){
-            if((j==0)||(j==2*n-2))
-            cout<<n;
-            else
-            cout<<n-1;
+            l=j;
+            r=(2*n-2)-j;
+            t=i;
+            d=(2*n-2)-i;
+            if(l<=r && l<=t && l<=d)
+            cout<<(4-l);
+            else if(r<=l && r<=t && r<=d )
+            cout<<(4-r);
+            else if(t<=l && t<=r && t<=d)
+            cout<<(4-t);
+            else if(d<=l && d<=r && d<=t)
+            cout<<(4-d);
 
+        
+
+            
         }
         cout<<endl;
 
-    
-      
-
-        }
-
-    cout<<string(2*n-1,'0'+n);
-        return 0;
+        
     }
+
+    
+
+        
+
+
+            
+
+           return 0;
+           
+        }
+    
