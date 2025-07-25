5439d89dc9c73f026b97b8c96fc33fc533466774
Devansh Trivedi <trivedidevansh1080@gmail.com>
2025-07-25 23:32:41 +0530

    finished stl

--- stl/algorithms.cpp
+++ stl/algorithms.cpp
@@ -7,10 +7,21 @@ int main() {
     std::cout<<std::count(mylist.begin(),mylist.end(),9)<<std::endl;
     std::vector<char>myvec{'b','d','a'};
     std::sort(myvec.begin(),myvec.end(),[](int a,int b){return a>b;});  //comparator is optional parameter unless dont want default ascending sort
-    for(auto i:myvec){
+   
+    //descending sort by comparator
+    std::for_each(mylist.begin(),mylist.end(),[](int x){std::cout<< x*x<<std::endl;});
+    std::vector<int>myvec2(6);
+    std::transform(mylist.begin(),mylist.end(),myvec2.begin(),[](int x){return x*x;});
+     for(auto i:myvec2){
         std::cout<<i<<std::endl;
     }
-    //descending sort by comparator
+    std::list<int>dev{5,7,9};
+    auto g=std::search(mylist.begin(),mylist.end(),dev.begin(),dev.end());
+    std::cout<<*(g)<<std::endl;
+    g++;
+    std::cout<<*(g);
+    
+
 
 
 
