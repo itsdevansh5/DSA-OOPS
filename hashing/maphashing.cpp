be265588e69aba03d822274747d7fdcb855e7386
Devansh Trivedi <trivedidevansh1080@gmail.com>
2025-08-02 11:22:40 +0530

    hashing

---
+++ hashing/maphashing.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+
+int main() {
+    std::map<int,int>hashmap;
+    int arr[12]={1,1,1,1,3,3,4,4,4,5,5,6};
+    for(int i=0;i<12;i++){
+        hashmap[arr[i]]++;
+    }
+    std::cout<<hashmap[4]<<" "<<hashmap[1]<<" "<<hashmap[7]<<std::endl;
+    for(auto i:hashmap){
+        std::cout<<i.first<<" "<<i.second<<std::endl;
+    }
+
+    return 0;
+}
