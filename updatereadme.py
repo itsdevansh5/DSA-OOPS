6b3f6fbcdf0847b77617da34f5cc83b51e917a75
Devansh Trivedi <trivedidevansh1080@gmail.com>
2025-08-25 14:34:09 +0530

    correct python file

--- updatereadme.py
+++ updatereadme.py
@@ -4,7 +4,7 @@ def list_folders(path='.'):
     folders = [f for f in os.listdir(path) if os.path.isdir(os.path.join(path, f)) and not f.startswith('.')]
     return folders
 
-with open("README.md", "w") as f:
+with open("readme.md", "w") as f:
     f.write("# C++ DSA & OOP Concepts Learned\n\n")
     f.write("## Concepts covered:\n\n")
     folders = list_folders()
