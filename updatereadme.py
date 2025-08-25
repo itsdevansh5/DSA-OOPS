9fe4b20591c3604d653cc6885830a29f92a488d6
Devansh Trivedi <trivedidevansh1080@gmail.com>
2025-08-25 14:32:47 +0530

    added workflow for readme automation

---
+++ updatereadme.py
@@ -0,0 +1,12 @@
+import os
+
+def list_folders(path='.'):
+    folders = [f for f in os.listdir(path) if os.path.isdir(os.path.join(path, f)) and not f.startswith('.')]
+    return folders
+
+with open("README.md", "w") as f:
+    f.write("# C++ DSA & OOP Concepts Learned\n\n")
+    f.write("## Concepts covered:\n\n")
+    folders = list_folders()
+    for folder in folders:
+        f.write(f"- {folder}\n")
